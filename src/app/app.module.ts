359edb017cb341a240e8614dae0d3f0e7c5b5257
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-25 02:14:49 +0530

    userregistration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
