723800904b6d397b4b4a89a65f1c3eae95e5ed00
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-28 14:30:20 +0530

    readadminlogin

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AdminLoginComponent {
 
+  username=""
+  password=""
+
+  readValues=()=>
+  {
+    let data:any={
+      "username":this.username,"password":this.password
+    }
+    console.log(data)
+  }
+
 }
